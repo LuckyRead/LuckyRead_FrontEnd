137fb7a9e3111e5db90404e914f9c464f9be86eb
Lizeth Daniela Castellanos Alfonso <ldcastellanosa@unal.edu.co>
2018-09-29 20:05:55 -0500

    basic body init

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import Init from './Init';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Init />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/Init.js
+++ src/components/Init.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from '../resources/logo.svg';
+import book from '../resources/black_book.png';
 import '../styles/Init.css';
+import { Button } from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+
 
 class Init extends Component {
-  render() {
-    return (
-      <div className="Init">
-        <header className="Init-header">
-          <img src={logo} className="Init-logo" alt="logo" />
-          <h1 className="Init-title">Welcome to React</h1>
-        </header>
-        <p className="Init-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    render() {
+  return (
+    <div class="content">
+    <div className="Init-header">
+      <div class="row">
+        <div class="col-md-6">
+          <h1 className="Init-title">Bienvenido a LuckyRead</h1>
+        <img src={book} className="Init-book" alt="book" />
+        </div>
+        <div class="col-md-6" align= "center">
+
+          <div class="row">
+            <h3 >Encontraras una lectura interesante.</h3>
+          </div>
+          <div class="row">
+            <Button className="Init-Button" color="warning">
+              Haz clic para empezar
+            </Button>{' '}
+          </div>
+        </div>
       </div>
-    );
-  }
+  </div>
+</div>
+  );
+}
 }
 
 export default Init;
