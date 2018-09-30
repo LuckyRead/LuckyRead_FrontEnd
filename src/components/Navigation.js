8bbe7d5ce8b6e657e71b06eea8d42f809261400b
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2018-09-29 23:21:46 -0500

    navbar added, without login

---
+++ src/components/Navigation.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+        };
+    this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+    render() {
+        return (
+            <Router>
+                <Navbar color="indigo" dark expand="md" scrolling>
+                    <NavbarBrand href="/">
+                        <strong>LuckyRead</strong>
+                    </NavbarBrand>
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem active>
+                              <NavLink to="#">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Ayuda</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Nosotros</NavLink>
+                          </NavItem>
+
+                        </NavbarNav>
+                        <NavbarNav left>
+                          <NavItem>
+                            <form className="form-inline md-form mt-0">
+                              <input className="form-control mr-sm-2 mb-0 text-black" type="text" placeholder="Search" aria-label="Search"/>
+                            </form>
+                          </NavItem>
+                          </NavbarNav>
+
+                        <NavbarNav right>
+                          <NavItem>
+                            <Dropdown>
+                                <DropdownToggle nav caret>Iniciar Sesion</DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem href="#">Action</DropdownItem>
+                                    <DropdownItem href="#">Another Action</DropdownItem>
+                                    <DropdownItem href="#">Something else here</DropdownItem>
+                                    <DropdownItem href="#">Something else here</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
+}
+export default Navigation
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import Init from './components/Init';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route} from 'react-router-dom'
 
-ReactDOM.render(<Init />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+         <div>
+             <Route exact path='/' component={Init} />
+         </div>
+  </BrowserRouter>
+  , document.getElementById('root')
+);
+
 registerServiceWorker();
