03dae9c6dbc02723e917b0aa81f9285a4ed6507d
Laura Molina <ldmolinav@unal.edu.co>
2018-10-11 23:19:12 -0500

    link init to categories page

--- src/components/InitCenter.js
+++ src/components/InitCenter.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import book from '../resources/black_book.png';
-import CategoriesPage from './CategoriesPage';
 import '../styles/init.css';
 import '../styles/navbar.css';
 import { Button } from 'reactstrap';
@@ -25,9 +24,9 @@ class InitCenter extends Component {
   <h3 className="Init-subtitle"><p>¿Quieres leer algo pero no sabes qué?
     Aquí te ofrecemos textos interesantes al azar según tus gustos.
     ¡Podrás interactuar con otros usuarios y aprender algo nuevo cada día!</p></h3>
-  <button type="button" className="Init-button" tag={Link} to="/CategoriesPage">
+  <Button className="Init-button" tag={Link} to="/categoriesPage">
   <strong>Empezar</strong>
-  </button>
+  </Button>
 
 
     </div>
