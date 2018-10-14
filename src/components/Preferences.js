4526cf2a53b3a404cde62c3b0bb1baa5827fb867
Laura Molina <ldmolinav@unal.edu.co>
2018-10-14 13:46:06 -0500

    button with icon

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 import '../styles/preferences.css';
+
+//Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Preferences extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+      hidden: true
+    }
   }
+
+  myFunction = (e) => {
+  const button_class = e.target.className
+  console.log(button_class);
+
+  if (button_class == "btn btn-primary active focus"){
+    this.setState({hidden: false})
+  } else {
+    this.setState({hidden: true})
+  }
+}
 
 
   render() {
     return (
-      <div className="btn-group-toggle" data-toggle="buttons">
-        <label className="btn btn-primary ">
-          <input type="checkbox" autocomplete="off"/> {this.props.name}
-        </label>
+      <div>
+        <div className="btn-group-toggle" data-toggle="buttons">
+          <label className="btn btn-primary"  id = {this.props.name} onClick = {this.myFunction}>
+            <FontAwesomeIcon icon="check" size ="sm" hidden = {this.state.hidden}/>&nbsp;<input type="checkbox" autocomplete="off"/> {this.props.name}
+          </label>
+        </div>
       </div>
     );
   }
