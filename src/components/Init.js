3277b3128019b47dda6cd63f94436d3d49878b5d
Laura Molina <ldmolinav@unal.edu.co>
2018-10-14 10:42:53 -0500

    resolving conflicts

--- src/components/Init.js
+++ src/components/Init.js
@@ -1,23 +1,21 @@
 //Dependencies
 import React, { Component } from 'react';
 import book from '../resources/black_book.png';
-import SignInForm from './SignInForm';
+import SignUpForm from './SignUpForm';
 import '../styles/init.css';
 import '../styles/navbar.css';
 import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
 import InitCenter from './InitCenter';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
 //Data
 //import items from '../data/menu.js'
 //Icons
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { faSmile } from '@fortawesome/free-solid-svg-icons'
 import { faMeh } from '@fortawesome/free-solid-svg-icons'
 import { faFrown } from '@fortawesome/free-solid-svg-icons'
