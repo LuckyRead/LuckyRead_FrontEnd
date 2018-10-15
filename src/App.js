57caa5650672c0953189d0b953a168a27292329b
Lizeth Daniela Castellanos Alfonso <ldcastellanosa@unal.edu.co>
2018-10-15 14:59:43 -0500

    Route Parameters in FragmentPage and Fragment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
-import { Route, BrowserRouter } from 'react-router-dom';
+import {Route, BrowserRouter} from 'react-router-dom';
 import Init from './components/Init/Init';
 import About from './components/About/AboutPage';
 import Contact from './components/Contact/Contact';
 import SignUp from './components/SignUp_LogIn/SignUp';
 import LogIn from './components/SignUp_LogIn/LogIn';
-import HomePage from './components/HomePage/HomePage';
+import FragmentsPage from './components/FragmentsPage/FragmentsPage';
 import CategoriesPage from './components/CategoriesPage/CategoriesPage';
-import AllFragment from './components/FragmentsPage/AllFragment';
+import Fragment from './components/FragmentsPage/Fragment';
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Init}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route exact path='/signup' component={SignUp}/>
-          <Route exact path='/login' component={LogIn}/>
-          <Route exact path='/homepage' component={HomePage}/>
-          <Route exact path='/categoriespage' component={CategoriesPage}/>
-          <Route exact path='/FragmentsPage' component={AllFragment}/>
-
-        </div>
-      </BrowserRouter>
-    );
+    return (<BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Route exact="exact" path='/Init' component={Init}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+        <Route exact="exact" path='/signup' component={SignUp}/>
+        <Route exact="exact" path='/login' component={LogIn}/>
+        <Route exact="exact" path='/' component={FragmentsPage}/>
+        <Route exact="exact" path='/categoriespage' component={CategoriesPage}/>
+      <Route path='/:fragment_id' component={Fragment}/>
+      </div>
+    </BrowserRouter>);
   }
 }
 
