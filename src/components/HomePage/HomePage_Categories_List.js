d9151eff5b513b790c8190a4beb67140861cf72e
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2018-10-14 21:28:23 -0500

    add topic

--- src/components/HomePage/HomePage_Categories_List.js
+++ src/components/HomePage/HomePage_Categories_List.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 //Components
 import Navigation from '../Navigation';
 import HomeCenter from './HomeCenter';
-import HomePage_Categorie from './HomePage_Categorie';
+import HomePage_Category from './HomePage_Category';
+import AddTopic from './AddTopic';
 //styles
 import '../../styles/homepage.css';
 
@@ -13,7 +14,7 @@ import '../../styles/homepage.css';
 class HomePage_Categories_List extends Component {
 
   state ={
-    categories: [
+    topics: [
       {id: '1', name: 'Historia'},
       {id: '2', name: 'Ciencia'},
       {id: '3', name: 'Tema 3'},
@@ -21,14 +22,22 @@ class HomePage_Categories_List extends Component {
       {id: '3', name: 'Biografias'},
       {id: '3', name: 'Emprendimiento'},
     ]
-
   }
+
+ addTopic = (topic) => {
+   topic.id = Math.random();
+   let topics = [...this.state.topics, topic];
+   this.setState({
+     topics: topics
+   });
+ }
 
   render(){
     return(
   <div className="row">
     <div className="col-sm-12" id="HomePage_Categories_List">
-      <HomePage_Categorie categories={this.state.categories}/>
+      <HomePage_Category topics={this.state.topics}/>
+    <AddTopic addTopic={this.addTopic} />
       </div>
       </div>
     )
---
+++ src/components/HomePage/HomePage_Category.js
@@ -0,0 +1,52 @@
+//Dependencies
+import React from 'react';
+
+//Components
+import Navigation from '../Navigation';
+import HomeCenter from './HomeCenter';
+//styles
+import '../../styles/homepage.css';
+
+//Data
+
+const HomePage_Categorie =({topics}) => {
+    // const topicList = topics.map (topic => {
+    //   if(topic.id==1){
+    //   return(
+    //     <div className="topic" key={topic.id}>
+    //     <div className="row">
+    //       <div className="col-sm-12" >
+    //       <button onClick={this.handleClick} id="CategoryButton"
+    //         onMouseOver={this.handleMouseOver}>{topic.name}</button>
+    //       </div>
+    //       </div>
+    //       </div>
+    //   )
+    // } else{
+    //   return null
+    // }
+    // })
+
+
+    const topicList = topics.map(topic =>{
+      return topic.id>=0 ? (
+            <div className="topic" key={topic.id}>
+            <div className="row">
+              <div className="col-sm-12" >
+              <button onClick={this.handleClick} id="CategoryButton"
+                onMouseOver={this.handleMouseOver}>{topic.name}</button>
+              </div>
+              </div>
+              </div>
+      ) : null
+    })
+
+
+    return(
+      <div className="topic-list">
+        {topicList}
+      </div>
+    )
+}
+
+export default HomePage_Categorie
