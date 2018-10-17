6e92442103e8cc8a353e2e72d9c8651f641e873b
Laura Molina <ldmolinav@unal.edu.co>
2018-10-16 22:59:58 -0500

    solve conflicts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-
+import React, {Component} from 'react';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Init from './components/Init/Init';
+import About from './components/About/AboutPage';
+import Contact from './components/Contact/Contact';
+import SignUp from './components/SignUp_LogIn/SignUpPage';
+import LoginPage from './components/SignUp_LogIn/LoginPage';
+import FragmentsPage from './components/FragmentsPage/FragmentsPage';
+import Categories_List from './components/CategoriesPage/Categories_List';
+import Fragment from './components/FragmentsPage/Fragment';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSmile } from '@fortawesome/free-solid-svg-icons'
@@ -6,41 +16,8 @@ import { faMeh } from '@fortawesome/free-solid-svg-icons'
 import { faFrown } from '@fortawesome/free-solid-svg-icons'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
-class App extends Component {
-  render() {
-    return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar/>
-        <Switch>
-          <Route exact path='/' component={Init}/>
-          <Route path='/about' component={About}/>
-          <Route path='/contact' component={Contact}/>
-          <Route path='/signup' component={SignUp}/>
-          <Route path='/login' component={LoginPage}/>
-          <Route path='/fragmentspage' component={FragmentsPage}/>
-          <Route path='/categoriespage' component={Categories_List}/>
-          <Route path='/:fragment_id' component={Fragment}/>
-        </Switch>
-      </div>
-    </BrowserRouter>);
-  }
-}
 
-library.add(faSmile);
-library.add(faMeh);
-library.add(faFrown);
-library.add(faCheck);
-
-
-export default App;
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSmile } from '@fortawesome/free-solid-svg-icons'
-import { faMeh } from '@fortawesome/free-solid-svg-icons'
-import { faFrown } from '@fortawesome/free-solid-svg-icons'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import requireAuth from './utils/requireAuth';
 
 
 class App extends Component {
@@ -55,12 +32,9 @@ class App extends Component {
           <Route path='/contact' component={Contact}/>
           <Route path='/signup' component={SignUp}/>
           <Route path='/login' component={LoginPage}/>
-          <Route path='/fragmentspage' component={FragmentsPage}/>
-          <Route path='/categoriespage' component={Categories_List}/>
-          <Route path='/:fragment_id' component={Fragment}/>
           <Route path='/fragmentspage' component={requireAuth(FragmentsPage)}/>
           <Route path='/categoriespage' component={requireAuth(Categories_List)}/>
-          <Route path='/:fragment_id' component={Fragment}/>
+          <Route path='/:fragment_id' component={requireAuth(Fragment)}/>
         </Switch>
       </div>
     </BrowserRouter>);
