14e9cd7e5cfeef64a4089e50032d6866f049c60f
Laura Molina <ldmolinav@unal.edu.co>
2018-10-27 09:32:23 -0500

    solve problems

---
+++ src/components/FragmentsPage/FragmentPDF.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import API from '../../api';
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom'
+import '../../styles/fragmentcontainer.css';
+class FragmentPDF extends Component {
+  state = {
+    post: null
+  }
+
+  componentDidMount(){
+    console.log('pdf page');
+    let id = this.props.match.params.fragment_id;
+    API.get('/api/fragments/'+id+'.pdf')
+      .then(res => {
+        console.log('pdf')
+        console.log(res)
+        this.setState({
+          post: res.data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className= "container-pdf">
+        <br/>
+        <div className="row row justify-content-center" id="PDF">
+          <iframe src="http://www.slideshare.net/slideshow/embed_code/key/fZHbg6xSXHL9Ua?startSlide=32" width="595" height="795" frameBorder="0" marginWidth="0" marginHeight="0" scrolling="no" allowFullScreen> </iframe>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FragmentPDF
