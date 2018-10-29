7ad5bb04920d0127ad6ddabcf0ebde7ece23265c
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2018-10-29 00:29:56 -0500

    SPRINT_REVISION_3

--- src/components/Profile/ProfileLeftInfo.js
+++ src/components/Profile/ProfileLeftInfo.js
@@ -6,9 +6,15 @@ import axios from 'axios';
 
 class ProfileLeftInfo extends Component {
 
+  state = {
+    posts: []
+  }
+
+
   constructor(props){
     super(props);
     this.state = {
+
         path: null
     };
   }
@@ -21,10 +27,40 @@ class ProfileLeftInfo extends Component {
       console.log(response['request']['responseURL'])
       this.setState({ path: response['request']['responseURL']});
     });
+
+    axios({
+      method: 'GET', url: 'https://luckyread-backend.herokuapp.com/user/info',
+      headers:
+      {
+        Authorization: "Bearer " + localStorage.jwtToken
+      }
+    }).then(
+
+      (response) => {
+      console.log(response)
+      this.setState({
+        posts: response["data"]
+
+      });
+      console.log(this.state);
+      console.log(response.data.title);
+
+    },
+    (err) => {
+      console.log('el error es pidiendo fragmento random')
+    }
+  );
+
+
+
+
+
   }
 
   render() {
+    const {posts} = this.state
     return (
+
       <div className="row">
       <div id="divProfile">
         <div className="card" >
@@ -32,7 +68,7 @@ class ProfileLeftInfo extends Component {
             <img className="rounded-circle" id="profileImage" width="150" height="150" src={this.state.path} alt=""></img>
           </a>
           <div className = "card-body">
-            <h4 className="card-title">Perrito Sorprendido</h4>
+            <h4 className="card-title">Usuario</h4>
             <h6 className="card-title">@PerritoLindo</h6>
           </div>
           <ul className="list-group list-group-flush" >
