90dedfea0371ea66a5b4ff05c74f23cbfacacee1
Lizeth Daniela Castellanos Alfonso <ldcastellanosa@unal.edu.co>
2018-11-11 02:48:42 -0500

    LandingPage added

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,8 @@ import { history } from "../_helpers";
 import { alertActions } from "../_actions";
 import { PrivateRoute } from "../_components";
 import { RegisterPage } from "../screens/RegisterPage";
+import LandingPage from "../screens/LandingPage";
+
 import "./App.css";
 
 class App extends Component {
@@ -22,20 +24,16 @@ class App extends Component {
   render() {
     const { alert } = this.props;
     return (
-      <div className="jumbotron">
-        <div className="container">
-          <div className="col-sm-8 col-sm-offset-2">
-            {alert.message && (
-              <div className={`alert ${alert.type}`}>{alert.message}</div>
-            )}
-            <Router history={history}>
-              <div>
-                {" "}
-                <Route path="/register" component={RegisterPage} />
-              </div>
-            </Router>
+      <div className="container-fluid">
+        {alert.message && (
+          <div className={`alert ${alert.type}`}>{alert.message}</div>
+        )}
+        <Router history={history}>
+          <div>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/register" component={RegisterPage} />
           </div>
-        </div>
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import "./index.css";
 import { store } from "./_helpers";
 import { App } from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 render(
   <Provider store={store}>
@@ -16,4 +16,4 @@ render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
