2e74b766328eb27355b6a4dab828125c6f13254b
Camilo Sanchez <caasanchezcr@unal.edu.co>
2018-11-12 22:03:17 -0500

    Landing Page refactored, changed to Styled component and reacstrap

--- src/screens/LandingPage/LandingPage.js
+++ src/screens/LandingPage/LandingPage.js
@@ -1,35 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./landingpage.css";
+//import "./landingpage.css";
+import { LandingPageContainer } from "./Styled";
+import { Row, Col, Jumbotron, Button } from "reactstrap";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div className="container-fluid">
-        <div className="jumbotron" id="Init">
-          <div className="row">
-            <div className="col-md-12" id="InitPanel">
-              <strong>
-                <h1 id="Init-title">Bienvenido a LuckyRead</h1>
-              </strong>
-              <hr />
-              <h3 className="Init-subtitle">
-                <p>
-                  ¿Quieres leer algo pero no sabes qué? Aquí te ofrecemos textos
-                  interesantes al azar según tus gustos. ¡Podrás interactuar con
-                  otros usuarios y aprender algo nuevo cada día!
-                </p>
-              </h3>
-              <Link to="/RegisterPage">
-                <button className="button button5" id="Init-button">
+    <LandingPageContainer>
+      <Jumbotron fluid id="Init">
+        <Row>
+          <Col xs="12" id="InitPanel">
+            <strong>
+              <h1 id="Init-title">Bienvenido a LuckyRead</h1>
+            </strong>
+            <hr />
+            <h3 className="Init-subtitle">
+              <p>
+                ¿Quieres leer algo pero no sabes qué? Aquí te ofrecemos textos
+                interesantes al azar según tus gustos. ¡Podrás interactuar con
+                otros usuarios y aprender algo nuevo cada día!
+              </p>
+            </h3>
+            <Link to="/RegisterPage">
+              <Button color="primary" id="Init-button">
+                <h4 id="readyText">
                   <strong>Empezar</strong>
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                </h4>
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Jumbotron>
+    </LandingPageContainer>
   );
 };
 
--- src/themes/colors.js
+++ src/themes/colors.js
@@ -5,7 +5,9 @@ const Palette = {
   green: " #7ef20f ",
   bluelight: "#0ff2bd",
   bluesea: " #0fddf2 ",
-  bluestrong: " #2b5693 "
+  bluestrong: " #2b5693 ",
+  blackopacity: "#0006",
+  blueTitleLanding: "#105f78"
 };
 
 export { Palette };
