c1a502604ffb01996624aa8290228a42ae41a0eb
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2018-11-25 18:36:32 -0500

    MakeComment section finished

--- src/common/MakeComment/MakeComment.js
+++ src/common/MakeComment/MakeComment.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Palette, MakeCommentContainer, TitleContainer } from "./Styled";
-import { Row, Col, Button } from "reactstrap";
+import { Row, Col, Button, FormGroup, Form, Input } from "reactstrap";
 
 export default class Comment extends React.Component {
   render() {
     return (
       <MakeCommentContainer>
         <TitleContainer>Realiza un comentario</TitleContainer>
+        <Form>
+          <FormGroup>
+            <Input type="textarea" name="text" id="comment" />
+          </FormGroup>
+          <Button color="info" style={{ height: "30px" }}>
+            <h6>Publicar</h6>
+          </Button>
+        </Form>
       </MakeCommentContainer>
     );
   }
--- src/common/MakeComment/Styled.js
+++ src/common/MakeComment/Styled.js
@@ -7,11 +7,16 @@ const MakeCommentContainer = styled.div`
   padding-left: 2% !important;
   margin: 1%;
   border-radius: 10px;
+  h6 {
+    font-size: 90%;
+  }
 `;
 
 const TitleContainer = styled.div`
   width: 100%;
   color: white;
   font-size: 20px;
+  margin-bottom: 1%;
 `;
+
 export { Palette, MakeCommentContainer, TitleContainer };
