92506fab5553ca5e31eef4ac0343a9a64a409768
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2018-11-25 19:30:51 -0500

    Ready to upload a new fragment

--- src/screens/ProfilePage/FragmentTab.js
+++ src/screens/ProfilePage/FragmentTab.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
-import { Row, Col, Button } from "reactstrap";
-import { TabContent } from "./Styled.js";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  InputGroup,
+  InputGroupText,
+  InputGroupAddon,
+  Input,
+  FormText
+} from "reactstrap";
+import { TabContent, TitleContainer } from "./Styled.js";
 
 export default class FragmentTab extends Component {
   render() {
     return (
       <TabContent>
-        <h1>Test</h1>
+        <h4 style={{ textAlign: "center", marginBottom: "2%" }}>
+          ¡Crea tu propio fragmento!
+        </h4>
+        <Form>
+          <TitleContainer>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>Titulo</InputGroupText>
+              </InputGroupAddon>
+              <Input />
+            </InputGroup>
+          </TitleContainer>
+          <FormGroup>
+            <Label for="introduccion">Introduccion</Label>
+            <Input type="textarea" name="introduccion" id="introduccion" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="contenido">Contenido</Label>
+            <Input type="textarea" name="contenido" id="contenido" />
+          </FormGroup>
+          <Button color="info" style={{ marginRight: "1%" }}>
+            Agregar imagen
+          </Button>
+          <Button color="info">Subir fragmento</Button>
+        </Form>
       </TabContent>
     );
   }
